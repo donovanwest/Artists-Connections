359ab703a942fda547c1675c43dfda760363dbfa
Donovan West <donovanrwest@gmail.com>
2020-08-30 14:06:22 -0500

    initial comit

---
+++ ArtistsConnectionsForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace ArtistsConnectionsFramework
+{
+    partial class ArtistsConnectionsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ListBox1 = new System.Windows.Forms.ListBox();
+            this.ListBox2 = new System.Windows.Forms.ListBox();
+            this.ListBox3 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ListBox1
+            // 
+            this.ListBox1.FormattingEnabled = true;
+            this.ListBox1.HorizontalScrollbar = true;
+            this.ListBox1.Location = new System.Drawing.Point(12, 52);
+            this.ListBox1.Name = "ListBox1";
+            this.ListBox1.Size = new System.Drawing.Size(204, 329);
+            this.ListBox1.TabIndex = 0;
+            // 
+            // ListBox2
+            // 
+            this.ListBox2.FormattingEnabled = true;
+            this.ListBox2.HorizontalScrollbar = true;
+            this.ListBox2.Location = new System.Drawing.Point(222, 52);
+            this.ListBox2.Name = "ListBox2";
+            this.ListBox2.Size = new System.Drawing.Size(214, 329);
+            this.ListBox2.TabIndex = 1;
+            // 
+            // ListBox3
+            // 
+            this.ListBox3.FormattingEnabled = true;
+            this.ListBox3.HorizontalScrollbar = true;
+            this.ListBox3.Location = new System.Drawing.Point(442, 52);
+            this.ListBox3.Name = "ListBox3";
+            this.ListBox3.Size = new System.Drawing.Size(219, 329);
+            this.ListBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(222, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(442, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // ArtistsConnectionsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 393);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ListBox3);
+            this.Controls.Add(this.ListBox2);
+            this.Controls.Add(this.ListBox1);
+            this.Name = "ArtistsConnectionsForm";
+            this.Text = "Artists\' Connections";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ListBox1;
+        private System.Windows.Forms.ListBox ListBox2;
+        private System.Windows.Forms.ListBox ListBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ArtistsConnectionsForm.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SpotifyAPI.Web;
+using Microsoft;
+using Microsoft.Msagl.Drawing;
+using System.Threading;
+using Microsoft.Msagl.Core.DataStructures;
+using Priority_Queue;
+
+namespace ArtistsConnectionsFramework
+{
+    public partial class ArtistsConnectionsForm : Form
+    {
+        SpotifyClient Spotify;
+        Graph ArtistsGraph = new Graph("Artists' Connections");
+        SimplePriorityQueue<string> ArtistsToCheck = new SimplePriorityQueue<string>();
+        List<string> CheckedArtists = new List<string>();
+        Dictionary<string, string> ArtistIdToName = new Dictionary<string, string>();
+        //HashSet<Edge> Edges = new HashSet<Edge>();
+
+        public ArtistsConnectionsForm()
+        {
+            InitializeComponent();
+            var config = SpotifyClientConfig.CreateDefault().WithAuthenticator(new ClientCredentialsAuthenticator("e6bf2e305d98443190c472ee318fd511", "96bad35ecf9c41f581a761eb3a85348b"));
+            Spotify = new SpotifyClient(config);
+            ArtistsToCheck.Enqueue("4JxdBEK730fH7tgcyG3vuv", 0);
+            int n = 0;
+            while (ArtistsToCheck.Count > 0 && ArtistsToCheck.GetPriority(ArtistsToCheck.First) <= 2)
+            {
+                string first = ArtistsToCheck.First;
+                float priority = ArtistsToCheck.GetPriority(first);
+                ArtistsToCheck.Dequeue();                   
+                Task<List<string>> artistsTask = FindAllConnectedArtists(first, priority);
+                artistsTask.Wait();
+                n++;                        
+            }
+            List<string> edges = new List<string>();
+            foreach(Edge edge in ArtistsGraph.Edges)
+            {
+                string sourceName;
+                string targetName;
+                ArtistIdToName.TryGetValue(edge.Source, out sourceName);
+                ArtistIdToName.TryGetValue(edge.Source, out targetName);
+                edges.Add(sourceName + " - " + edge.LabelText + " - " + targetName);
+            }
+            ListBox1.DataSource = edges;
+            while (ArtistsToCheck.Count > 0)
+            {
+                string name;
+                ArtistIdToName.TryGetValue(ArtistsToCheck.First, out name);
+                float priority = ArtistsToCheck.GetPriority(ArtistsToCheck.First);
+                ArtistsToCheck.Dequeue();
+                ListBox3.Items.Add(name + " - " + priority);
+            }
+            foreach (string id in CheckedArtists)
+            {
+                string name;
+                ArtistIdToName.TryGetValue(id, out name);
+                ListBox2.Items.Add(name);
+            }
+            label1.Text = ListBox1.Items.Count.ToString();
+            label2.Text = ListBox2.Items.Count.ToString();
+            label3.Text = ListBox3.Items.Count.ToString();
+        }
+
+        public Task<List<string>> FindAllConnectedArtists(string artistID, float priority)
+        {
+            return Task.Run(() =>
+            {
+                CheckedArtists.Add(artistID);
+                List<string> artistNames = new List<string>();
+                HashSet<string> connectedArtists = new HashSet<string>();
+                Paging<SimpleAlbum> pagingAlbums;
+                try
+                {
+                    Task<Paging<SimpleAlbum>> pagingAlbumsTask = Spotify.Artists.GetAlbums(artistID);
+                    pagingAlbumsTask.Wait();
+                    pagingAlbums = pagingAlbumsTask.Result;
+                } catch {
+                    Thread.Sleep(3500);
+                    Task<Paging<SimpleAlbum>> pagingAlbumsTask = Spotify.Artists.GetAlbums(artistID);
+                    pagingAlbumsTask.Wait();
+                    pagingAlbums = pagingAlbumsTask.Result;
+                }                       
+                List<string> albumIDs = new List<string>();           
+                foreach (SimpleAlbum simpleAlbum in pagingAlbums.Items)
+                {
+                    albumIDs.Add(simpleAlbum.Id);
+                }
+                if (albumIDs.Count > 0)
+                {
+                    AlbumsRequest request = new AlbumsRequest(albumIDs);
+                    AlbumsResponse albumsResponse;
+                    try
+                    {
+                        Task<AlbumsResponse> fullAlbumTask = Spotify.Albums.GetSeveral(request);
+                        fullAlbumTask.Wait();
+                        albumsResponse = fullAlbumTask.Result;
+                    }
+                    catch
+                    {
+                        Thread.Sleep(3500);
+                        Task<AlbumsResponse> fullAlbumTask = Spotify.Albums.GetSeveral(request);
+                        fullAlbumTask.Wait();
+                        albumsResponse = fullAlbumTask.Result;
+                    }
+
+                    foreach (FullAlbum fullAlbum in albumsResponse.Albums)
+                    {
+                        Paging<SimpleTrack> simpleTracks = fullAlbum.Tracks;
+                        foreach (SimpleTrack simpleTrack in simpleTracks.Items)
+                        {
+                            List<SimpleArtist> simpleArtistsList = simpleTrack.Artists;
+                            List<String> simpleArtistsOnTrack = new List<string>();
+                            foreach (SimpleArtist simpleArtist in simpleArtistsList)
+                                simpleArtistsOnTrack.Add(simpleArtist.Id);
+                            if (simpleArtistsOnTrack.Contains(artistID))
+                            {
+                                foreach (SimpleArtist simpleArtist in simpleTrack.Artists)
+                                {
+                                    if (!ArtistIdToName.ContainsKey(simpleArtist.Id))
+                                        ArtistIdToName.Add(simpleArtist.Id, simpleArtist.Name);
+                                    if (!ArtistsToCheck.Contains(simpleArtist.Id) && !CheckedArtists.Contains(simpleArtist.Id))
+                                    {
+                                        ArtistsToCheck.Enqueue(simpleArtist.Id, priority + 1);
+                                        Edge edge = new Edge(artistID, simpleTrack.Name, simpleArtist.Id);
+                                        if (!containsEdge(edge))
+                                        {
+                                            ArtistsGraph.AddEdge(artistID, simpleTrack.Name, simpleArtist.Id);
+                                            //Edges.Add(edge);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                return connectedArtists.ToList();
+            });
+        }
+
+        public bool containsEdge(Edge edge)
+        {
+            foreach(Edge e in ArtistsGraph.Edges)
+            {
+                if (e.Source.Equals(edge.Source) && e.LabelText.Equals(edge.LabelText) && e.Target.Equals(edge.Target))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
